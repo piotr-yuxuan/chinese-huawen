a50a3641f23e4accb63ad816f41c0692e97bae43
piotr-yuxuan <pierre.deboisset@telecom-paristech.fr>
2015-08-02 11:22:49 -1000

    formal grammar seems to be done

--- clojure/華文/src/華文/lexer/parser.clj
+++ clojure/華文/src/華文/lexer/parser.clj
@@ -43,20 +43,157 @@
     IDC2 = '⿰' | '⿱' | '⿴' | '⿵' | '⿶' | '⿷' | '⿸' | '⿹' | '⿺' | '⿻'
     IDC3 = '⿲' | '⿳'
     IDC = #'\\P{InIdeographic_Description_Characters}'
-    L = #'\\P{InIdeographic_Description_Characters}' (* letter *)"))
+    L = #'\\P{InIdeographic_Description_Characters}' (* anything else *)"))
 ;; (as-and-bs "⿰⿱ab⿱cd")
 ;; => [:S [:F [:IDC2 "⿰"] [:F [:IDC2 "⿱"] [:L "a"] [:L "b"]] [:F [:IDC2 "⿱"] [:L "c"] [:L "d"]]]]
 
-;; Last try, still to be tried :-)
-(def as-and-bs
+;; The following lines show something like tdd, but the repl prevented me from properly writing the unitary tests. Þ They should be added.
+
+(def grammar
   (insta/parser
-   "S = F+ (* Parsing tree may have not head *)
-    F = ((IDC3 (L | F)) | IDC2) (L | F) (L | F) (* form *)
+   "S = ((IDC3 (L | S)) | IDC2) (L | S) (L | S)
     IDC2 = '⿰' | '⿱' | '⿴' | '⿵' | '⿶' | '⿷' | '⿸' | '⿹' | '⿺' | '⿻'
     IDC3 = '⿲' | '⿳'
     IDC = #'\\p{InIdeographic_Description_Characters}'
-    L = #'\\P{InIdeographic_Description_Characters}' (* letter *)"))
-;; Here we want to have a map
+    L = #'\\P{InIdeographic_Description_Characters}'"))
+;; Finally we can use this grammar:
 (defn parse-ids
-  [str]
-  "lol")
+  [i]
+  (let [ids (nth (split i) 2)]
+    (println ids)
+    (grammar ids)))
+
+(def grammar
+  (insta/parser
+   "S = S"))
+
+;; However, this doesn't work on radical thus we need to enlarge the starting rule.
+(def grammar
+  (insta/parser
+   "S = Form | (Form Id <(''|Sep)>)+
+    Form =    Letter
+              | (((IDC3 (Letter|S)) | IDC2) (Letter|S) (Letter|S))
+    Id =      <'['> #'[A-Z]' <']'>
+    Sep =     '\t'
+    IDC2 =    '⿰' | '⿱' | '⿴' | '⿵' | '⿶' | '⿷' | '⿸' | '⿹' | '⿺' | '⿻'
+    IDC3 =    '⿲' | '⿳'
+    Code =    #'&[A-Z0-9-]+;' (* for not-yet-implemented code points*)
+
+    (* Same as Han but with code and without IDC *)
+    Letter =  Code | CJK | CJKA | CJKB | CJKC | CJKD | CJKE | CJKRS | KR
+              | CJKSP | CJKS | ECJKLM | CJKCo | CJKCI | CJKCF | CJKCIS
+    (* As should be defined \\p{Han} *)
+    Han =     CJK | CJKA | CJKB | CJKC | CJKD | CJKE | CJKRS | KR | IDC | CJKSP
+              | CJKS | ECJKLM | CJKCo | CJKCI | CJKCF | CJKCIS
+
+    (* Ideographs blocks follow *)
+    CJK =     #'[\u4e00-\u9fff]'
+    CJKA =    #'[\u3400-\u4dbf]'
+    CJKB =    #'[\u20000-\u2a6df]'
+    CJKC =    #'[\u2a700-\u2b73f]'
+    CJKD =    #'[\u2b740-\u2B81F]'
+    CJKE =    #'[\u2b820-\u2ceaf]'
+    CJKRS =   #'[\u2e80-\u2eff]'
+    KR =      #'[\u2f00-\u2fdf]'
+    IDC =     #'[\u2ff0-\u2fff]'
+    CJKSP =   #'[\u3000-\u303f]'
+    CJKS =    #'[\u31c0-\u31ef]'
+    ECJKLM =  #'[\u3200-\u32ff]'
+    CJKCo =   #'[\u3300-\u33ff]'
+    CJKCI =   #'[\uf900-\ufaff]'
+    CJKCF =   #'[\ufe30-\ufe4f]'
+    CJKCIS =  #'[\u2f800-\u2fa1f]'"
+   :output-format :hiccup ; or :enlive
+   ))
+
+;; The previous one is pretty advanced but we would like to deal better with Id
+;; and get the result somewhat lighter.
+(def grammar
+  (insta/parser
+   "S =       Letter | Form+
+    Form =    (((IDC3 (Letter|S)) | IDC2) (Letter|S) (Letter|S) Id* <Sep*>)
+    Id =      <'['> #'[A-Z]' <']'>
+    Sep =     '\t'
+    IDC2 =    '⿰' | '⿱' | '⿴' | '⿵' | '⿶' | '⿷' | '⿸' | '⿹' | '⿺' | '⿻'
+    IDC3 =    '⿲' | '⿳'
+    Code =    #'&[A-Z0-9-]+;' (* for not-yet-implemented code points*)
+
+    (* Same as Han but with code and without IDC *)
+    Letter =  Code | CJK | CJKA | CJKB | CJKC | CJKD | CJKE | CJKRS | KR
+              | CJKSP | CJKS | ECJKLM | CJKCo | CJKCI | CJKCF | CJKCIS
+    (* As should be defined \\p{Han} *)
+    Han =     CJK | CJKA | CJKB | CJKC | CJKD | CJKE | CJKRS | KR | IDC | CJKSP
+              | CJKS | ECJKLM | CJKCo | CJKCI | CJKCF | CJKCIS
+
+    (* Ideographs blocks follow *)
+    <CJK> =     #'[\u4e00-\u9fff]'
+    <CJKA> =    #'[\u3400-\u4dbf]'
+    <CJKB> =    #'[\u20000-\u2a6df]'
+    <CJKC> =    #'[\u2a700-\u2b73f]'
+    <CJKD> =    #'[\u2b740-\u2B81F]'
+    <CJKE> =    #'[\u2b820-\u2ceaf]'
+    <CJKRS> =   #'[\u2e80-\u2eff]'
+    <KR> =      #'[\u2f00-\u2fdf]'
+    IDC =     #'[\u2ff0-\u2fff]'
+    <CJKSP> =   #'[\u3000-\u303f]'
+    <CJKS> =    #'[\u31c0-\u31ef]'
+    <ECJKLM> =  #'[\u3200-\u32ff]'
+    <CJKCo> =   #'[\u3300-\u33ff]'
+    <CJKCI> =   #'[\uf900-\ufaff]'
+    <CJKCF> =   #'[\ufe30-\ufe4f]'
+    <CJKCIS> =  #'[\u2f800-\u2fa1f]'"
+   :output-format :hiccup ; or :enlive
+   ))
+
+;; And finally we would like to really get a tree structure and not something flattened. Moreover we solve the last problem: Id, which was actually not previously solved :/
+;; Actually the Id problem accepts more than one solution. We choose to change the data structure (IDC vs. :Version) but remember we must address it properly. Indeed it won't be trivial to deal with nested diffrent versions. Another solution is to keep data homogeneous: the parsed collection returned contains one or 2*i elements. If one then no problem; if 2*i, then 0 mod 2 are version and 1 mod 2 are version ids. This data structure is more appealing for a Clojure mindset but from my umble current viewpoint, :Version will warn very well the user the latter has to deal with the former.
+;; In addition to this, ⿰&CDP-88F0;&CDP-8C4E;	⿰&CDP-8B5E;&CDP-8C4E; is a good counter-example for the flat solution. So basically you just have to count: if the output is counted more than 1, then we'll deal with versions.
+;; Same explanation for :Code.
+(def grammar
+  (insta/parser
+   "<S> =       Letter | (Version <Sep*>)+ | Form
+    Sep =       '\t' | ' '*
+    Version =   Form Id* (* needed because <S> *)
+    <Form> =    ⿰ | ⿱ | ⿴ | ⿵ | ⿶ | ⿷ | ⿸ | ⿹ | ⿺ | ⿻ | ⿲ | ⿳
+    Id =        <'['> #'[A-Z]+' <']'>
+    ⿰ = <'⿰'> IDC2
+    ⿱ = <'⿱'> IDC2
+    ⿴ = <'⿴'> IDC2
+    ⿵ = <'⿵'> IDC2
+    ⿶ = <'⿶'> IDC2
+    ⿷ = <'⿷'> IDC2
+    ⿸ = <'⿸'> IDC2
+    ⿹ = <'⿹'> IDC2
+    ⿺ = <'⿺'> IDC2
+    ⿻ = <'⿻'> IDC2
+    ⿲ = <'⿲'> IDC3
+    ⿳ = <'⿳'> IDC3
+
+    <IDC2> =  (Letter|S) (Letter|S)
+    <IDC3> =  (Letter|S) (Letter|S) (Letter|S)
+
+    (* Same as Han but with code and without IDC *)
+    <Letter> =  Code | CJK | CJKA | CJKB | CJKC | CJKD | CJKE | CJKRS | KR
+                | CJKSP | CJKS | ECJKLM | CJKCo | CJKCI | CJKCF | CJKCIS
+    Code =      #'&[A-Z0-9-]+;' (* for not-yet-implemented code points*)
+    (* As should be defined \\p{Han} *)
+    Han =       CJK | CJKA | CJKB | CJKC | CJKD | CJKE | CJKRS | KR | IDC | CJKSP
+                | CJKS | ECJKLM | CJKCo | CJKCI | CJKCF | CJKCIS
+
+    (* Ideographs blocks follow *)
+    <CJK> =     #'[\u4e00-\u9fff]'
+    <CJKA> =    #'[\u3400-\u4dbf]'
+    <CJKB> =    #'[\u20000-\u2a6df]'
+    <CJKC> =    #'[\u2a700-\u2b73f]'
+    <CJKD> =    #'[\u2b740-\u2B81F]'
+    <CJKE> =    #'[\u2b820-\u2ceaf]'
+    <CJKRS> =   #'[\u2e80-\u2eff]'
+    <KR> =      #'[\u2f00-\u2fdf]'
+    IDC =       #'[\u2ff0-\u2fff]'
+    <CJKSP> =   #'[\u3000-\u303f]'
+    <CJKS> =    #'[\u31c0-\u31ef]'
+    <ECJKLM> =  #'[\u3200-\u32ff]'
+    <CJKCo> =   #'[\u3300-\u33ff]'
+    <CJKCI> =   #'[\uf900-\ufaff]'
+    <CJKCF> =   #'[\ufe30-\ufe4f]'
+    <CJKCIS> =  #'[\u2f800-\u2fa1f]'"))
