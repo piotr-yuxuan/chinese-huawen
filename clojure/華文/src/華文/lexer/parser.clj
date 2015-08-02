ecade5321e333ee3cb01d2cd5d4e76967afdd6b5
piotr-yuxuan <pierre.deboisset@telecom-paristech.fr>
2015-08-02 11:34:27 -1000

    typo

--- clojure/華文/src/華文/lexer/parser.clj
+++ clojure/華文/src/華文/lexer/parser.clj
@@ -1,3 +1,5 @@
+;; Here development is incremental so jump to the end to skip incremental changes.
+
 (ns 華文.lexer.parser
   (:require [instaparse.core :as insta]))
 
@@ -146,16 +148,18 @@
    ))
 
 ;; And finally we would like to really get a tree structure and not something flattened. Moreover we solve the last problem: Id, which was actually not previously solved :/
-;; Actually the Id problem accepts more than one solution. We choose to change the data structure (IDC vs. :Version) but remember we must address it properly. Indeed it won't be trivial to deal with nested diffrent versions. Another solution is to keep data homogeneous: the parsed collection returned contains one or 2*i elements. If one then no problem; if 2*i, then 0 mod 2 are version and 1 mod 2 are version ids. This data structure is more appealing for a Clojure mindset but from my umble current viewpoint, :Version will warn very well the user the latter has to deal with the former.
-;; In addition to this, ⿰&CDP-88F0;&CDP-8C4E;	⿰&CDP-8B5E;&CDP-8C4E; is a good counter-example for the flat solution. So basically you just have to count: if the output is counted more than 1, then we'll deal with versions.
+;; Actually the Id problem accepts more than one solution. We choose to change the data structure (IDC vs. :Variant) but remember we must address it properly. Indeed it won't be trivial to deal with nested different variants. Another solution is to keep data homogeneous: the parsed collection returned contains one or 2*i elements. If one then no problem; if 2*i, then 0 mod 2 are variants and 1 mod 2 are variant ids. This data structure is more appealing for a Clojure mindset but from my umble current viewpoint, :Variant will warn very well the user the latter has to deal with the former.
+;; In addition to this, ⿰&CDP-88F0;&CDP-8C4E;	⿰&CDP-8B5E;&CDP-8C4E; is a good counter-example for the flat solution. So basically you just have to count: if the output is counted more than 1, then we'll deal with variants. They sometimes are anonymous.
 ;; Same explanation for :Code.
 (def grammar
   (insta/parser
-   "<S> =       Letter | (Version <Sep*>)+ | Form
+   "<S> =       Letter | (Variant <Sep*>)+ | Form
     Sep =       '\t' | ' '*
-    Version =   Form Id* (* needed because <S> *)
+    Variant =   Form Id* (* needed because <S> *)
     <Form> =    ⿰ | ⿱ | ⿴ | ⿵ | ⿶ | ⿷ | ⿸ | ⿹ | ⿺ | ⿻ | ⿲ | ⿳
-    Id =        <'['> #'[A-Z]+' <']'>
+    Id =        <'['> #'[A-Z]'+ <']'> (* Assuming one letter per version *)
+
+    (* Operators *)
     ⿰ = <'⿰'> IDC2
     ⿱ = <'⿱'> IDC2
     ⿴ = <'⿴'> IDC2
@@ -169,18 +173,20 @@
     ⿲ = <'⿲'> IDC3
     ⿳ = <'⿳'> IDC3
 
+    (* Operator arities *)
     <IDC2> =  (Letter|S) (Letter|S)
     <IDC3> =  (Letter|S) (Letter|S) (Letter|S)
 
+    (* Operands *)
     (* Same as Han but with code and without IDC *)
     <Letter> =  Code | CJK | CJKA | CJKB | CJKC | CJKD | CJKE | CJKRS | KR
                 | CJKSP | CJKS | ECJKLM | CJKCo | CJKCI | CJKCF | CJKCIS
     Code =      #'&[A-Z0-9-]+;' (* for not-yet-implemented code points*)
-    (* As should be defined \\p{Han} *)
+    (* As should be defined \\p{Han}. Currently unused *)
     Han =       CJK | CJKA | CJKB | CJKC | CJKD | CJKE | CJKRS | KR | IDC | CJKSP
                 | CJKS | ECJKLM | CJKCo | CJKCI | CJKCF | CJKCIS
 
-    (* Ideographs blocks follow *)
+    (* Ideographs blocks *)
     <CJK> =     #'[\u4e00-\u9fff]'
     <CJKA> =    #'[\u3400-\u4dbf]'
     <CJKB> =    #'[\u20000-\u2a6df]'
